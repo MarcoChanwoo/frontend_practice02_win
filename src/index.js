7341891632d6b2de691664080658db8bed5900fa
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-09 21:26:57 +0900

    3rd commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
-import { createStore } from 'redux';
-import rootReducer from './modules/index';
+import { createStore, applyMiddleware } from 'redux';
+// import rootReducer, { rootSaga } from './modules/index';
+import rootReducer, { rootSaga } from './modules';
 import { composeWithDevTools } from '../node_modules/redux-devtools-extension/index';
 import { Provider } from '../node_modules/react-redux/es/exports';
 // import { createStore } from '../node_modules/redux/index';
 // import rootReducer from './modules';
 // import { composeWithDevTools } from '@redux-devtools/extension';
+import createSagaMiddleware from 'redux-saga';
+// import { applyMiddleware } from '../node_modules/redux/index';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,16 @@
 // import { combineReducers } from '../../node_modules/redux/index'; <- 아래와 같이 그냥 'redux'로 설정할 것
 import { combineReducers } from 'redux';
-import auth from './auth';
+import auth, { authSaga } from './auth';
+import loading from './loading';
+import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
     auth,
+    loading,
 });
+
+export function* rootSaga() {
+    yield all([authSaga]);
+}
 
 export default rootReducer;
