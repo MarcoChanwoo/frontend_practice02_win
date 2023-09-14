1dfa24bfec5f8df2735504f6e9a338d9d83716fb
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-14 15:11:36 +0900

    6th

--- src/pages/GuestPage.js
+++ src/pages/GuestPage.js
@@ -5,31 +5,35 @@ import field02 from '../img/field02.png';
 import Button from '../components/common/Button';
 
 const Whitebox = styled.div`
-    font-size: 1.4rem;
+    font-size: 1.1rem;
     text-align: center;
     .field {
         width: 25rem;
         height: 16rem;
+    }
+    .button {
+        margin-top: 1rem;
+        margin-bottom: 2rem;
     }
 `;
 
 const GuestPage = () => {
     return (
         <Whitebox>
-            <div>
-                <HeaderNormal />
-                <div className="body">
-                    전국의 현장 상황을 놓치지않고 보호합니다
-                    <img className="field" alt="field01" src={field01} />
-                    <br />
-                    깨끗함과 안전함을 추구합니다
-                    <img className="field" alt="field01" src={field02} />
-                    <br />
-                    <br />
-                    지금 상담해보세요!
-                    <br />
-                    <Button>상담하러 가기</Button>
-                </div>
+            <HeaderNormal />
+            <div className="body">
+                전국의 현장 상황을 놓치지않고 보호합니다
+                <br />
+                <img className="field" alt="field01" src={field01} />
+                <br />
+                깨끗함과 안전함을 추구합니다
+                <br />
+                <img className="field" alt="field01" src={field02} />
+                <br />
+                <br />
+                지금 상담해보세요!
+                <br />
+                <Button className="button">상담하러 가기</Button>
             </div>
         </Whitebox>
     );
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,48 @@
+import styled from 'styled-components';
 import Header from '../components/common/Header';
+import field03 from '../img/field03.png';
+import field04 from '../img/field04.png';
+import field05 from '../img/field05.png';
+
+const Container = styled.div`
+    text-align: center;
+    .hello {
+        font-size: 1.6rem;
+        font-weight: bold;
+    }
+    .main {
+        margin-top: 3rem;
+        padding-bottom: 2rem;
+        font-size: 1.1rem;
+    }
+    .img {
+        width: 25rem;
+        height: 16rem;
+        padding-bottom: 0.5rem;
+    }
+`;
 
 const PostListPage = () => {
     return (
-        <>
+        <Container>
             <Header />
-            <div>XXX</div>
-        </>
+            <div className="hello">HY상사 주식회사에 오신것을 환영합니다!</div>
+            <div className="main">
+                우리는 만듭니다
+                <br />
+                <img className="img" src={field03} alt="field03" />
+                <br />
+                근로자의 안전을 확실하게!
+                <br />
+                <img className="img" src={field04} alt="field04" />
+                <br />
+                우리가 모여 안전한 건물을 보장합니다!
+                <br />
+                <img className="img" src={field05} alt="field05" />
+                <br />
+                문의 전화: 02) 123-4567
+            </div>
+        </Container>
     );
 };
 
